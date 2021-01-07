9e8f312c5ec8e72d9ab0f11416d3b83c98b78744
sameghosts <seanfeiner.brown@gmail.com>
2021-01-07 11:44:47 -0800

    functionality of listeners and even color change no restriction of non number characters

--- js/main.js
+++ js/main.js
@@ -1,25 +1,42 @@
 
 //selectors 
 const form = document.querySelector('#myForm');
-const inputVal = document.getElementById('submitText');
+// let inputVal = document.getElementById("submitText").value;
+const plus= document.getElementById("addButton");
+const minus= document.getElementById('subButton');
 let valueDisp = document.getElementById('display');
 
 //value for operations
 //input value to be added or subtracted
 let enData;
 //current number / value displayed
-let disData;
+let disData =0;
 // result of data through operation\
 let resData;
 //functions for changing input value (enData) to new value (resData) and return it by changing the display of the display tag
-let addValue = () =>{
-
-}
-
-let subButton = () =>{
-
+let checkData = () =>{
+  if(disData < 0){
+    document.getElementById("displayData").style.color = "red";
+  }
 }
 
 //event listener for button style change
-form.addEventListener("#addButton", addValue());
-form.addEventListener("subButton" subValu());
+plus.addEventListener('click', function() {
+  enData = parseInt(document.getElementById("submitText").value);
+  resData = enData + disData;
+  disData = resData;
+  console.log(disData);
+  document.getElementById("displayData").innerHTML =`${disData}`
+  checkData();
+  
+});
+minus.addEventListener('click', function() {
+  enData = parseInt(document.getElementById("submitText").value);
+  resData = disData-enData;
+  disData = resData;
+  console.log(disData);
+  document.getElementById("displayData").innerHTML =`${disData}`
+  checkData();
+});
+
+// form.addEventListener("subButton", subValue());
