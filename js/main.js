59033420ee480e4f4ef65e472db3d0cf3479513d
sameghosts <seanfeiner.brown@gmail.com>
2021-01-07 09:56:57 -0800

    some javascript skeleton with psuedo

---
+++ js/main.js
@@ -0,0 +1,25 @@
+
+//selectors 
+const form = document.querySelector('#myForm');
+const inputVal = document.getElementById('submitText');
+let valueDisp = document.getElementById('display');
+
+//value for operations
+//input value to be added or subtracted
+let enData;
+//current number / value displayed
+let disData;
+// result of data through operation\
+let resData;
+//functions for changing input value (enData) to new value (resData) and return it by changing the display of the display tag
+let addValue = () =>{
+
+}
+
+let subButton = () =>{
+
+}
+
+//event listener for button style change
+form.addEventListener("#addButton", addValue());
+form.addEventListener("subButton" subValu());
