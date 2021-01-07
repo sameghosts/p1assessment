96511c57cf5310a979f1f5f85f55bad24d4f89b2
sameghosts <seanfeiner.brown@gmail.com>
2021-01-07 11:53:27 -0800

    boolean update

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ let resData;
 let checkData = () =>{
   if(disData < 0){
     document.getElementById("displayData").style.color = "red";
-  } else if(disData >0){
+  } else if(disData >=0){
     document.getElementById("displayData").style.color = "black";
   }
 }
